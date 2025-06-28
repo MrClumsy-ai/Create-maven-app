de6f95621a2b80aeadb40b3eaf4aded8800a023c
MrClumsy-ai <eduardomenchaca01@hotmail.com>
2025-06-28 13:33:56 -0600

    batman: create maven app

---
+++ test.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+name="my-app"
+company="awesomecompany"
+
+while getopts "hn:c:" flag; do
+    case $flag in
+        h)
+            echo "program -n name"
+            ;;
+        n)
+            name=$OPTARG
+            ;;
+        c)
+            company=$OPTARG
+            ;;
+        \?)
+            echo "invalid option: -$OPTARG" >&2
+            exit 1
+            ;;
+    esac
+done
+
+echo "name = $name"
+echo "company = $company"
+if [ "$name" = "test" ]; then
+    echo "the name cannot be 'test'"
+    exit 1
+fi
+
+if [ "$company" = "test" ]; then
+    echo "the name of the company cannot be 'test'"
+    exit 1
+fi
+
+# mvn archetype:generate -DgroupId=com.$company.app -DartifactId=$name -DarchetypeArtifactId=maven-archetype-quickstart -DarchetypeVersion=1.5 -DinteractiveMode=false
